89088a69313d0fe535aa76ed28d05f01726702c5
Tomas.Rinkis <Tomas.Rinkis@gmail.com>
2014-07-15 08:20:24 -0700

    CustomNS library in C++

---
+++ breakIntoObjC/AppleCoder-OpenGLES-01/Classes/CustomNS/CSNObject.cpp
@@ -0,0 +1,9 @@
+//
+//  CSNObject.cpp
+//  AppleCoder-OpenGLES-00
+//
+//  Created by Brolis on 7/15/14.
+//
+//
+
+#include "CSNObject.h"
---
+++ breakIntoObjC/AppleCoder-OpenGLES-01/Classes/CustomNS/CSNObject.h
@@ -0,0 +1,14 @@
+//
+//  CSNObject.h
+//  AppleCoder-OpenGLES-00
+//
+//  Created by Brolis on 7/15/14.
+//
+//
+
+#ifndef __AppleCoder_OpenGLES_00__CSNObject__
+#define __AppleCoder_OpenGLES_00__CSNObject__
+
+#include <iostream>
+
+#endif /* defined(__AppleCoder_OpenGLES_00__CSNObject__) */
